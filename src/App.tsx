41bcba5ae9925be6014875929db44124c9e2f985
EV1L46rus <vislik46rus@gmail.com>
2023-02-22 01:04:14 +0300

    test

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Card from './components/Card'
+
+function App() {
+  return (
+    <div>
+      <Card width='200px' height='200px'>
+        <button>test</button>
+      </Card>
+    </div>
+  )
+}
+
+export default App
