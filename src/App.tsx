c2d977f26d228b03d7570bb44e316152740c4d94
EV1L46rus <vislik46rus@gmail.com>
2023-02-23 17:17:36 +0300

    done

--- src/App.tsx
+++ src/App.tsx
@@ -6,49 +6,20 @@ import List from './components/List'
 import TodoItem from './components/TodoItem'
 import UserItem from './components/UserItem'
 import { ITodo, IUser } from './types/types'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import UserPage from './components/UserPage'
+import TodosPage from './components/TodosPage'
 
 function App() {
-  const [users, setUsers] = useState<IUser[]>([])
-  const [todos, setTodos] = useState<ITodo[]>([])
-
-  useEffect(()=>{
-    fetchUsers()
-    fetchTodos()
-  }, [])
-
-  async function fetchUsers() {
-    try {
-      const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users')
-      setUsers(response.data)
-    } catch (error) {
-      alert(error)
-    }
-  }
-
-  async function fetchTodos() {
-    try {
-      const response = await axios.get<ITodo[]>('https://jsonplaceholder.typicode.com/todos?_limit=10')
-      setTodos(response.data)
-    } catch (error) {
-      alert(error)
-    }
-  }
 
   return (
-    <div>
-      <EventsExample/>
-      <Card width='200px' height='200px' variant={CardVatiant.primary}>
-        <button>test</button>
-      </Card>
-      <List
-        items={users}
-        renderItem={(user: IUser) => <UserItem user={user} key={user.id}/>}
-      />
-      <List
-        items={todos}
-        renderItem={(todo:ITodo) => <TodoItem todo={todo} key={todo.id}/>}
-      />
-    </div>
+    <BrowserRouter>
+      <Routes>
+              <Route path="/users" element={<UserPage/>}/>
+              <Route path="/todos" element={<TodosPage/>}/>
+              <Route path="*" element={<div>NotFound</div>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
